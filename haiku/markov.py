8a511b098f9872112a5b096621e7799d26fd034a
nathanlubchenco <nathanlubchenco@gmail.com>
2016-02-14 01:04:53 -0700

    working v 0.1 -- prints 3 line *haikus*

--- haiku/markov.py
+++ haiku/markov.py
@@ -1,5 +1,6 @@
 from textblob import TextBlob
 from nltk.tokenize import BlanklineTokenizer
+import random
 
 with open('basho.txt', 'r') as content_file:
     content = content_file.read()
@@ -7,7 +8,8 @@ with open('basho.txt', 'r') as content_file:
 #print content
 
 tokenizer = BlanklineTokenizer()
-corpus = TextBlob(content,  tokenizer=tokenizer)
+cleaned_content = content.lower()
+corpus = TextBlob(cleaned_content,  tokenizer=tokenizer)
 
 haiku = corpus.sentences
 #print haiku
@@ -16,7 +18,7 @@ bigrams = corpus.ngrams(n=2)
 trigrams = corpus.ngrams(n=3)
 
 #print bigrams
-
+dict = {}
 for bigram in bigrams:
     k = bigram[0]
     v = bigram[1]
@@ -28,4 +30,31 @@ for bigram in bigrams:
     else:
         dict[k] = { v : 1}
 
-print dict
+#print dict
+
+def weighted_choice(map):
+    choices = [] 
+    for k in map:
+        #print k 
+        for n in range(1, map[k] + 1):
+            choices.append(k)
+    #print choices
+    choice = random.choice(choices)
+    #print choice
+    return choice
+
+seed = random.choice(dict.keys())
+length = random.randint(11,15) 
+
+output = [seed]
+#print output
+for i in range(length):
+    output.append(weighted_choice(dict[output[i]]))
+
+print output[0:4]
+print output[4:9]
+print output[9:]
+
+
+
+
